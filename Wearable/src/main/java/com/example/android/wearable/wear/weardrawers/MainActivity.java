73c1ce4566bc73c4d32f067a2409f5bf17037d1b
google-automerger <automerger@google.com>
2019-04-11 13:59:05 -0700

    Auto-update

--- Wearable/src/main/java/com/example/android/wearable/wear/weardrawers/MainActivity.java
+++ Wearable/src/main/java/com/example/android/wearable/wear/weardrawers/MainActivity.java
@@ -23,10 +23,6 @@ import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.support.wear.ambient.AmbientModeSupport;
-import android.support.wear.widget.drawer.WearableActionDrawerView;
-import android.support.wear.widget.drawer.WearableNavigationDrawerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -34,6 +30,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.fragment.app.FragmentActivity;
+import androidx.wear.ambient.AmbientModeSupport;
+import androidx.wear.widget.drawer.WearableActionDrawerView;
+import androidx.wear.widget.drawer.WearableNavigationDrawerView;
 
 import java.util.ArrayList;
 
